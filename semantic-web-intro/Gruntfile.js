85cf19195f4ec8d9031f2b9074aafc7751210eff
Fannon <heimlersimon@gmail.com>
2014-05-12 12:56:40 +0200

    swi

--- semantic-web-intro/Gruntfile.js
+++ semantic-web-intro/Gruntfile.js
@@ -1,73 +1,72 @@
 /* global module:false */
 module.exports = function(grunt) {
-	var port = grunt.option('port') || 8000;
-	// Project configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		meta: {
-			banner:
-				'/*!\n' +
-				' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
-				' * http://lab.hakim.se/reveal-js\n' +
-				' * MIT licensed\n' +
-				' *\n' +
-				' * Copyright (C) 2014 Hakim El Hattab, http://hakim.se\n' +
-				' */'
-		},
+    var port = grunt.option('port') || 8000;
+    // Project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        meta: {
+            banner: '/*!\n' +
+                ' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
+                ' * http://lab.hakim.se/reveal-js\n' +
+                ' * MIT licensed\n' +
+                ' *\n' +
+                ' * Copyright (C) 2014 Hakim El Hattab, http://hakim.se\n' +
+                ' */'
+        },
 
-		sass: {
-			main: {
-				files: {
-					'../_themes/fannon/fannon.css': '../_themes/fannon/source/fannon.scss'
+        sass: {
+            main: {
+                files: {
+                    '../_themes/fannon/fannon.css': '../_themes/fannon/source/fannon.scss'
 
-				}
-			}
-		},
+                }
+            }
+        },
 
-		jshint: {
-			options: {
-				curly: false,
-				eqeqeq: true,
-				immed: true,
-				latedef: true,
-				newcap: true,
-				noarg: true,
-				sub: true,
-				undef: true,
-				eqnull: true,
-				browser: true,
-				expr: true,
+        jshint: {
+            options: {
+                curly: false,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                eqnull: true,
+                browser: true,
+                expr: true,
                 jquery: true,
-				globals: {
-					head: false,
-					module: false,
-					console: false,
-					unescape: false
-				}
-			},
-			files: ['js/presentation.js' ]
-		},
+                globals: {
+                    head: false,
+                    module: false,
+                    console: false,
+                    unescape: false
+                }
+            },
+            files: ['js/presentation.js']
+        },
 
-		connect: {
-			server: {
-				options: {
-					port: port,
-					base: '.',
+        connect: {
+            server: {
+                options: {
+                    port: port,
+                    base: '.',
                     livereload: true
-				}
-			}
-		},
+                }
+            }
+        },
 
-		zip: {
-			'reveal-js-presentation.zip': [
-				'index.html',
-				'css/**',
-				'js/**',
-				'lib/**',
-				'images/**',
-				'plugin/**'
-			]
-		},
+        zip: {
+            'download.zip': [
+                'index.html',
+                'css/**',
+                'js/**',
+                'lib/**',
+                'images/**',
+                'plugin/**'
+            ]
+        },
 
         clean: {
             temp: {
@@ -110,47 +109,47 @@ module.exports = function(grunt) {
             }
         },
 
-		watch: {
-			main: {
-				files: [ 'Gruntfile.js', 'js/reveal.js', 'css/reveal.css' ],
-				tasks: 'default'
-			},
-			theme: {
-				files: [ '../_themes/fannon/source/fannon.scss'],
-				tasks: 'themes'
-			},
-			livereload: {
+        watch: {
+            main: {
+                files: ['Gruntfile.js', 'js/reveal.js', 'css/reveal.css'],
+                tasks: 'default'
+            },
+            theme: {
+                files: ['../_themes/fannon/source/fannon.scss'],
+                tasks: 'themes'
+            },
+            livereload: {
                 files: ['css/theme/*.css', 'index.html'],
                 options: {
                     livereload: true
                 }
             }
-		}
+        }
 
-	});
+    });
 
-	// Dependencies
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-contrib-sass' );
-    grunt.loadNpmTasks( 'grunt-contrib-connect' );
-    grunt.loadNpmTasks( 'grunt-contrib-copy' );
-	grunt.loadNpmTasks( 'grunt-contrib-clean' );
-	grunt.loadNpmTasks( 'grunt-zip' );
+    // Dependencies
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-zip');
 
     // Serve presentation locally
-    grunt.registerTask( 'default', ['sass', 'copy:themes', 'connect', 'watch' ] );
+    grunt.registerTask('default', ['sass', 'copy:themes', 'connect', 'watch']);
 
-	// Default task
-	grunt.registerTask( 'build', ['copy:themes', 'jshint', 'themes', 'clean'] );
+    // Default task
+    grunt.registerTask('build', ['copy:themes', 'jshint', 'themes', 'clean']);
 
-	// Theme task
-	grunt.registerTask( 'themes', [ 'sass', 'copy:themes' ] );
+    // Theme task
+    grunt.registerTask('themes', ['sass', 'copy:themes']);
 
-	// Package presentation to archive
-	grunt.registerTask( 'package', [ 'build', 'zip' ] );
+    // Package presentation to archive
+    grunt.registerTask('package', ['build', 'zip']);
 
-	// Run tests
-	grunt.registerTask( 'test', [ 'jshint'] );
+    // Run tests
+    grunt.registerTask('test', ['jshint']);
 
 };
